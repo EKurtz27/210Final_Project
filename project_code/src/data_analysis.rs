356fe165ba5903812236e9ba096a7498776de6e8
EvanK <evan_kurtz@outlook.com>
2025-04-30 15:05:35 -0400

    Minor update: removed duplicate test that was not deleted when tests moved into a seperate module

--- project_code/src/data_analysis.rs
+++ project_code/src/data_analysis.rs
@@ -46,40 +46,6 @@ pub fn viewership_distribution (cliques: &Vec<Vec<NodeStats>>) -> Vec<Vec<(u32, 
     }
     return all_view_dists
 }
-#[test]
-fn test_distributions () {
-    let node1 = NodeStats {
-        new_id: 1,
-        views: 100,
-        mature: true,
-        partner: false,
-    };
-    let node2 = NodeStats {
-        new_id: 2,
-        views: 200,
-        mature: true,
-        partner: false,
-    };
-    let node3 = NodeStats {
-        new_id: 3,
-        views: 300,
-        mature: true,
-        partner: false,
-    };  
-    let clique1: Vec<NodeStats> = vec![node1, node2, node3];
-    let clique2: Vec<NodeStats> = vec![node1, node3];
-    let cliques = vec![clique1, clique2];
-
-    let dists = viewership_distribution(&cliques);
-    let true_dists: Vec<Vec<(u32, f32)>> = vec![
-        vec![(1, 100.0/600.0), (2, 200.0/600.0), (3, 300.0/600.0)], 
-        vec![(1, 100.0/400.0), (3, 300.0/400.0)]
-        ];
-
-    assert_eq!(dists, true_dists, "Distributions not aligning");
-
-}
-
 
 use plotters::prelude::*;
 /// Uses plotters to generate barcharts of the distribution of viewership statistics for each clique \
